4b0bad52127560b949bb6f13af9318850f247faa
heinzaa <52783798+heinzaa@users.noreply.github.com>
2021-11-30 23:14:58 +0100

    - first

---
+++ webapp/index.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/m/Text"
+
+], function (Text) {
+	"use strict";
+
+
+
+    new Text({
+        text: "Hello Marc"
+    }).placeAt("content") // content ist die ID in der HTML File
+
+	
+});
