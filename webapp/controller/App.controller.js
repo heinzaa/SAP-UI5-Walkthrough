5914a7b0d8dabb917719db350b2c4eb0c2fc5d09
heinzaa <52783798+heinzaa@users.noreply.github.com>
2021-12-01 15:34:05 +0100

    Step 14

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,26 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
- ], function (Controller, MessageToast, JSONModel) {
+    "sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-       onInit : function () {
-          // set data model on view
-          var oData = {
-             recipient : {
-                name : "World"
-             }
-          };
-
-          // Model erzeugen
-          var oModel = new JSONModel(oData);
-          // Model an die View binden, damit diese Access darauf hat
-          this.getView().setModel(oModel);
-       },
        onShowHello : function () {
-          MessageToast.show("Hello World");
+          // read msg from i18n model
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+          // show message
+          MessageToast.show(sMsg);
        }
     });
  });
- 
